ef77d6a25fa4f1e75cbc19057b06964c973945db
Domenic Corso <domenic.corso@live.com>
2018-11-10 18:24:40 +1100

    Created Board and BoardPiece

--- src/classes/Board.ts
+++ src/classes/Board.ts
@@ -1,6 +1,22 @@
-class Board {
-    constructor (size: number) {
+import BoardPiece from './BoardPiece'
 
+class Board {
+    private size: number
+    private grid: BoardPiece[][]
+
+    constructor (size: number) {
+        this.size = size
+        this.grid = []
+        
+        // Construct grid
+        for (let x = 0; x < this.size; x++) {
+            const a: BoardPiece[] = []
+            this.grid.push(a)
+
+            for (let y = 0; y < this.size; y++) {
+                a.push(null)
+            }
+        }
     }
 }
 
---
+++ src/classes/BoardPiece.ts
@@ -0,0 +1,5 @@
+class BoardPiece {
+
+}
+
+export default BoardPiece
