f3f613dcffbd2e174ccb022937115e40adf2cbb5
Domenic Corso <domenic.corso@live.com>
2018-11-11 00:16:41 +1100

    Implemented 'place' for App

--- src/classes/App.ts
+++ src/classes/App.ts
@@ -1,8 +1,38 @@
 import Robot from './Robot'
 import Board from './Board'
+import BoardDirection from '../enums/BoardDirection';
+import BoardPiece from './BoardPiece';
 
 class App {
-    
+    readonly boardSize: number = 5
+
+    private board: Board
+    private robotPiece: BoardPiece
+    private robot: Robot
+
+    constructor () {
+        this.board = new Board(this.boardSize)
+        this.robot = new Robot()
+        this.robotPiece = new BoardPiece(this.robot)
+    }
+
+    /**
+     * Places the robot on a given (x, y) coordinate, facing a given
+     * direction.
+     * 
+     * @param  {number} x
+     * @param  {number} y
+     * @param  {BoardDirection} direction
+     * @returns void
+     */
+    public place (x: number, y: number, direction: BoardDirection): void {
+        if (!this.board.inBounds(x, y)) {
+            return
+        }
+
+        this.robotPiece.direction = direction
+        this.board.placePiece(this.robotPiece, x, y)
+    }
 }
 
 export default App
