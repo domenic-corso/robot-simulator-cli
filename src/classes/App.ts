4577e859bcc9eb218a830f1fd223f3f1fd40caa5
Domenic Corso <domenic.corso@live.com>
2018-11-11 00:39:07 +1100

    Implemented 'move' for App

--- src/classes/App.ts
+++ src/classes/App.ts
@@ -15,6 +15,42 @@ class App {
         this.robot = new Robot()
         this.robotPiece = new BoardPiece(this.robot)
     }
+    
+    /**
+     * Determines the difference in coordinates based on the
+     * current direction of the board piece
+     * 
+     * @returns x
+     */
+    private determineMoveDifference () : {x, y} {
+        switch (this.robotPiece.direction) {
+            case BoardDirection.North:
+            return {
+                x: 0,
+                y: -1
+            }
+            case BoardDirection.East:
+            return {
+                x: 1,
+                y: 0
+            }
+            case BoardDirection.South:
+            return {
+                x: 0,
+                y: 1
+            }
+            case BoardDirection.West:
+            return {
+                x: -1,
+                y: 0
+            }
+        }
+
+        return {
+            x: 0,
+            y: 0
+        }
+    }
 
     /**
      * Places the robot on a given (x, y) coordinate, facing a given
@@ -32,6 +68,28 @@ class App {
 
         this.robotPiece.direction = direction
         this.board.placePiece(this.robotPiece, x, y)
+    }
+    
+    /**
+     * Moves the board piece one square forward in the direction
+     * that it is facing.
+     * 
+     * @returns void
+     */
+    public move (): void {
+        if (!this.board.hasPiece(this.robotPiece)) {
+            return
+        }
+
+        // Determine the new coordinate for the board piece
+        const current = this.board.getCoordinatesOf(this.robotPiece)
+        const difference = this.determineMoveDifference()
+        const newCoordinate = {
+            x: current.x + difference.x,
+            y: current.y + difference.y
+        }
+
+        this.board.placePiece(this.robotPiece, newCoordinate.x, newCoordinate.y)
     }
 }
 
